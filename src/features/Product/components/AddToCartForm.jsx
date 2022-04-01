4bae04689e574fbf37e977f346b7cb8ad990a433
quangduc <quangduc2520@gmail.com>
2022-04-01 22:14:32 +0700

    fix add

--- src/features/Product/components/AddToCartForm.jsx
+++ src/features/Product/components/AddToCartForm.jsx
@@ -1,8 +1,9 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Button } from '@material-ui/core';
+import { Button, Dialog, DialogContentText, IconButton, makeStyles } from '@material-ui/core';
+import { Close } from '@material-ui/icons';
 import QuantityField from 'components/form-controls/QuantityField';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
@@ -10,7 +11,30 @@ AddToCartForm.propTypes = {
   onSubmit: PropTypes.func,
 };
 
+const useStyles = makeStyles((theme) => ({
+  dialog: {
+    padding: '40px 30px 30px',
+    color: theme.palette.primary.main,
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    color: theme.palette.grey[500],
+    zIndex: 1,
+  },
+}));
 function AddToCartForm({ onSubmit = null }) {
+  const [open, setOpen] = useState(false);
+  const classes = useStyles();
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const [canAdd,setCanAdd] = useState(false)
   const schema = yup.object().shape({
     quantity: yup
       .number()
@@ -26,18 +50,39 @@ function AddToCartForm({ onSubmit = null }) {
   });
 
   const handleSubmit = async (values) => {
-    if (onSubmit) {
+    if (onSubmit&& localStorage.length>0) {
+      setCanAdd(true)
       await onSubmit(values);
     }
+    
   };
 
   return (
     <form onSubmit={form.handleSubmit(handleSubmit)}>
       <QuantityField name="quantity" label="Quantity" form={form} />
 
-      <Button type="submit" variant="contained" color="primary" style={{ width: '250px' }} size="large">
+      <Button type="submit" variant="contained" color="primary" style={{ width: '250px' }} size="large"  onClick={handleOpen}>
         Add to cart
       </Button>
+{canAdd===false&&  <Dialog
+              disableBackdropClick
+              disableEscapeKeyDown
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="form-dialog-title"
+              
+            >
+              <IconButton className={classes.closeButton} >
+                <Close onClick={handleClose} />
+              </IconButton>
+
+             
+                <DialogContentText className={classes.dialog}>
+                  Hãy đăng nhập để tiếp tục mua sắm!
+                </DialogContentText>
+            
+              
+            </Dialog>}
     </form>
   );
 }
